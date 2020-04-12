66aab5587d36d4c797b50d1ad0792648fa00d24b
abramson21 <56224786+abramson21@users.noreply.github.com>
2020-04-12 22:31:03 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const mongoose = require('mongoose');
+
 
 const urls = require('./routes/index');
 
@@ -24,8 +26,11 @@ mongoose.connect(MONGODB, {
   useUnifiedTopology: true,
 });
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use(cookieParser());
 
